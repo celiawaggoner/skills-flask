457cf39bb02c303448690dd98333caa35d82179f
Celia Waggoner <celiawaggoner@Celias-MacBook-Pro.local>
2016-01-23 15:41:39 -0800

    Completed code, having issues with correct file paths

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,10 +9,29 @@ def index_page():
 
     return "<html><body>This is the homepage.</body></html>"
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+
+#show the application form HTML on this page
+@app.route("/application-form")
+def show_form():
+    """Shows application form"""
+
+    return render_template("application-form.html")
+
+#get responses from form, format them with Jinja and return a response
+@app.route("/application", methods=["POST"])
+def submit_form():
+    """Submit application form"""
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("salary")
+    job_title = request.form.get("jobtitle")
+
+    return render_template("application-responses.html",
+                            first_name=first_name,
+                            last_name=last_name,
+                            salary=salary,
+                            job_title=job_title)
 
 
 if __name__ == "__main__":
